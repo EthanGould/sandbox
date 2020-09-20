5644993b041b7e7b388e5dfd850df711cebd3504
Ethan Gould <ethan.g.907@gmail.com>
2020-09-20 14:32:26 -0400

    new skill cards layout, felt cute might delete later

--- me-2020/main.js
+++ me-2020/main.js
@@ -3,17 +3,46 @@ var module = {};
 module.init = () => {
     module.siteContent = document.querySelector('.section--sliced');
     module.siteMainContent = document.querySelector('.js-main-content');
+    module.cards = document.querySelectorAll('.js-card');
+    module.cardSpreadOffset = 450;
+
     module.events();
 }
 
 module.events = () => {
     window.addEventListener('scroll', module.handleScroll);
+
 };
 
-module.handleScroll = ()  =>{
-    if (module.siteContent.getBoundingClientRect().top <= 0) {
+module.handleScroll = ()  => {
+    let scrollTopContent = module.siteContent.getBoundingClientRect().top;
+    let scrollTopMainContent = module.siteMainContent.getBoundingClientRect().top;
+
+    if (scrollTopContent <= 0) {
         module.fadeContent();
     }
+
+    if (scrollTopMainContent <= module.cardSpreadOffset) {
+        module.spreadCards();
+    } else {
+        module.collapseCards();
+    }
+}
+
+module.spreadCards = () => {
+    if (!module.cards[0].classList.contains('stacked')) { return };
+
+    module.cards.forEach(card => {
+        card.classList.remove('stacked');
+    });
+}
+
+module.collapseCards = () => {
+    if (module.cards[0].classList.contains('stacked')) { return };
+
+    module.cards.forEach(card => {
+        card.classList.add('stacked');
+    });
 }
 
 module.fadeContent = () => {
