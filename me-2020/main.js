41517c23196338d7e0e2289b64f66bee51a8eac5
Ethan Gould <ethan.g.907@gmail.com>
2020-11-26 22:39:56 -0500

    new banner shape + scroll throttle

--- me-2020/main.js
+++ me-2020/main.js
@@ -11,11 +11,43 @@ module.init = () => {
     module.events();
 }
 
+module.debounce = (func, wait, immediate) => {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+module.throttle = (fn, wait) => {
+    var time = Date.now();
+    return function () {
+        if ((time + wait - Date.now()) < 0) {
+            fn();
+            time = Date.now();
+        }
+    }
+}
+
 module.events = () => {
+    // Returns a function, that, as long as it continues to be invoked, will not
+    // be triggered. The function will be called after it stops being called for
+    // N milliseconds. If `immediate` is passed, trigger the function on the
+    // leading edge, instead of the trailing.
+    
+    console.log('boop');
     window.addEventListener('scroll', module.handleScroll);
 };
 
-module.handleScroll = ()  => {
+module.handleScroll = module.throttle(()  => {
+    console.log('runing');
     let scrollTopContent = module.siteIntroContent.getBoundingClientRect().top;
     let scrollTopMainContent = module.siteMainContent.getBoundingClientRect().top;
 
@@ -28,22 +60,17 @@ module.handleScroll = ()  => {
     } else {
         module.collapseCards();
     }
-}
+}, 100);
 
 module.spreadCards = () => {
     if (!module.cards[0].classList.contains('stacked')) { return };
-
-    module.cards.forEach(card => {
-        card.classList.remove('stacked');
-    });
+    module.cards.forEach(card => card.classList.remove('stacked'));
 }
 
 module.collapseCards = () => {
     if (module.cards[0].classList.contains('stacked')) { return };
 
-    module.cards.forEach(card => {
-        card.classList.add('stacked');
-    });
+    module.cards.forEach(card => card.classList.add('stacked'));
 }
 
 module.fadeContent = () => {
