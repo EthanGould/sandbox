efd3cacf0dd06711484d9c934bee1d28ec88a6c2
Ethan Gould <ethan.g.907@gmail.com>
2021-06-17 03:12:04 -0400

    addedauto play option w slide interval config

--- slicker-slider/main.js
+++ slicker-slider/main.js
@@ -5,9 +5,10 @@ SS.init = (conf) => {
     SS.container = document.querySelector(SS.conf.el);
     SS.slides = document.querySelectorAll(`${SS.conf.el} > ul > li`);
     SS.slideList = document.querySelector(`${SS.conf.el} > ul`);
+    SS.currentSlide = 0;
 
     if (SS.container) {
-        SS.generateDots();
+        SS.generateNav();
         SS.layout();
     }
 }
@@ -16,25 +17,73 @@ SS.layout = () => {
     const slideWidth = `${SS.container.clientWidth / SS.conf.visibleSlideCount}px`;
     SS.slides.forEach(s => s.style.width = slideWidth);
     SS.slideList.classList.add('ss');
+
+    if (SS.conf.autoplay) {
+        SS.autoplay();
+    }
 }
 
 SS.gotoSlide = (e) => {
     const slide = SS.slides[e.target.dataset.slide];
-    slide.scrollIntoView();
-    slide.focus();
+    slide.scrollIntoView({behavior: 'smooth'});
+    SS.currentSlide = parseInt(e.target.dataset.slide, 10);
 }
 
-SS.generateDots = () => {
+SS.autoplay = () => {
+    // exit if pause button was clicked
+    if (!SS.conf.autoplay) { return }
+
+    SS.interval = setInterval(() => {
+        // Get next slide, or go back to the first slide
+        SS.currentSlide = SS.slides[SS.currentSlide + 1] ? SS.currentSlide + 1 : 0;
+        SS.slides[SS.currentSlide].scrollIntoView({behavior: 'smooth'});
+    }, SS.conf.interval || 3000);
+}
+
+SS.generateNav = () => {
+    const type = SS.conf.dotNavigation ? 'button' : 'a';
+    const dots = SS.generateDots(type);
+
+    if (SS.conf.autoplay) {
+        SS.playPause = SS.generateAutoplay();
+        SS.container.appendChild(SS.playPause);
+    }
+
+    SS.container.appendChild(dots);
+    SS.dots = document.querySelectorAll(`.ss__dots ${type}`);
+
+    // Setup event handlers after all dynamic slider elements are created
+    SS.eventHandlers();
+
+    // development only 
+    window.slider = { container: SS.container, slides: SS.slides, dots: SS.dots, configs: config };
+    // end development only
+}
+
+SS.generateAutoplay = () => {
+    const autoplayToggle = document.createElement('button');
+    autoplayToggle.classList.add('ss__play');
+    autoplayToggle.innerText = 'Pause';
+
+    return autoplayToggle;
+}
+
+SS.generateDots = (type) => {
     let dots = document.createElement('ul');
     dots.classList.add('ss__dots');
+
     SS.slides.forEach((s, i) => {
         let dot = document.createElement('li');
-        let indicator = document.createElement('button');
+        let indicator = document.createElement('a');
         let altText = s.firstElementChild.getAttribute('alt');
+
+        if (SS.conf.dotNavigation) {
+            indicator = document.createElement(type);
+            indicator.setAttribute('aria-label', `Goto slide ${i+1}, ${altText}`);
+        }
 
         indicator.dataset.slide = i;
         indicator.classList.add('ss__dot');
-        indicator.setAttribute('aria-label', `Goto slide ${i+1}, ${altText}`);
 
         if (i == 0) {
             indicator.classList.add('current');
@@ -43,15 +92,19 @@ SS.generateDots = () => {
         dot.appendChild(indicator);
         dots.appendChild(dot);
     });
-    SS.container.appendChild(dots);
-    SS.dots = document.querySelectorAll('.ss__dots button');
 
-    // Setup event handlers after all dynamic slider elements are created
-    SS.eventHandlers();
+    return dots;
+}
 
-    // development only 
-    window.slider = { container: SS.container, slides: SS.slides, dots: SS.dots, configs: config };
-    // end development only
+SS.toggleAutoplay = () => {
+    SS.conf.autoplay = !SS.conf.autoplay;
+    SS.playPause.innerText = SS.conf.autoplay ? 'Pause' : 'Play';
+
+    if (!SS.conf.autoplay) {
+        clearInterval(SS.interval); // Clear current autoplay
+    } else {
+        SS.autoplay(); // Begin a new autoplay
+    }
 }
 
 SS.setSlideDot = () => {
@@ -63,13 +116,34 @@ SS.setSlideDot = () => {
 
 SS.eventHandlers = () => {
     window.addEventListener('resize', SS.layout);
+
     SS.slideList.addEventListener('scroll', SS.setSlideDot); // #TODO throttle this event handler
-    SS.dots.forEach(d => d.addEventListener('click', SS.gotoSlide));
+
+    if (SS.conf.autoplay) {
+        SS.playPause.addEventListener('click', SS.toggleAutoplay);
+    }
+
+    if (SS.conf.dotNavigation) {
+        SS.dots.forEach(d => d.addEventListener('click', SS.gotoSlide));
+    }
 }
 
-let config = {
+const config = {
     el: '.ss__bound',
+    dotNavigation: true,
+    autoplay: true,
+    interval: 1000,
     visibleSlideCount: 1.25
 }
+
+/*
+======= OPTIONS =========
+el: string - selector
+dotNavigation: bool
+autoplay: bool
+interval: int (ms)
+visibleSlideCount: float
+==========================
+*/
 
 SS.init(config);
